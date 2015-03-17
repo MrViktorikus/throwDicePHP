9d454397556fb9f82e36200d0f3d14d306d82f8d
viktja710 <viktja710@E29621.edu.linkoping.se>
2015-03-17 14:00:42 +0100

    ta bort från cart fixat, kvar att fixa: om antalet produkter som skrivs in är högre än vad som finns ska det ej fungera.

--- printDice.php
+++ printDice.php
@@ -1,8 +1,48 @@
-
 <?php
+require_once "throwDice.php";
 
-$object = new stdClass();
-$object->name = 'Huturutu';
+$diceSides = 6;
+$diceAmount = 2;
 
-echo $object->name;
- 
+if (isset($_GET["diceSides"])) {
+    $diceSides = (int) filter_input(INPUT_GET, 'diceSides', FILTER_SANITIZE_SPECIAL_CHARS);
+    $diceAmount = (int) filter_input(INPUT_GET, 'diceAmount', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+$dice = new Dice($diceSides);
+$diceRolled = $dice->rollDice($diceAmount);
+
+function printRolledDices($rolls){
+    $orderedList = "<ol>";
+    foreach ($rolls as $roll){
+        $orderedList .= "<li>" . $roll . "</li>";
+    }
+    $orderedList .= "</ol>";
+    return $orderedList;
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Roll Dices</title>
+        <meta charset="utf8">
+    </head>
+    <body>
+        <form action="" method="GET">
+            <label for="diceAmount">Number of Dices:</label><br><input tyoe="text" name="diceAmount" id="diceAmount">
+            <br><br>
+            <label for="diceSides">Number of Sides:</label><br><input type="text" name="diceSides" id="diceSides">
+            <br>
+            <input type="submit" value="Roll Dices">
+        </form>
+        <h2>Rolled Dices</h2>
+        <ul>
+            <li>Amount of sides on the dice: <?php echo $diceRolled["diceSides"] ?></li>
+            <li>Amount of rolled dices: <?php echo $diceRolled["rolls"] ?></li>
+            <li>Sum of the rolled dices: <?php echo $diceRolled["sum"] ?></li>
+            <li>Rolls:
+                <?php echo printRolledDices($diceRolled["dice"]); ?>
+            </li>
+        </ul>
+    </body>
+</html>
--- throwDice.php
+++ throwDice.php
@@ -1,8 +1,24 @@
 <?php
+
 class Dice {
-    public $dice;
-    
-    public function sentence() {
-        
+
+    protected $diceSides;
+
+    public function __construct($diceSides = 6) {
+        $this->diceSides = $diceSides;
     }
+
+    public function rollDice($diceRoll = 1) {
+        $rolledDice = array();
+        for ($i = 0; $i < $diceRoll; $i++) {
+            $sideRolled = rand(1, $this->diceSides);
+            $rolledDice["dice"][] = $sideRolled;
+        }
+        $rolledDice["sum"] = array_sum($rolledDice["dice"]);
+        $rolledDice["sides"] = $this->diceSides;
+        $rolledDice["rolls"] = $diceRoll;
+
+        return rolledDice;
+    }
+
 }
